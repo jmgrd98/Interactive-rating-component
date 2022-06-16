78de08ac3df0c357a8bf80a1067e70d6bdae5396
João Marcelo Dantas <jmgrd98@gmail.com>
2022-06-16 19:44:38 -0300

    commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
+const submit = document.getElementById('submit')
+
+submit.addEventListener("click", select)
+
 function select(){
-    const element = document.querySelector('li')
-    var list = [document.querySelector('ul')]
+    const element = document.getElementsByTagName('label')
 
 element.style.backgroundColor = "hsl(25, 97%, 53%)";
 element.style.opacity = 1;
 
+
 }
+
