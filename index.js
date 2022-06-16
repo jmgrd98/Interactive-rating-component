437fd9813608a4ed3dc7019b2e2107ac9d2f0a58
João Marcelo Dantas <jmgrd98@gmail.com>
2022-06-16 16:19:37 -0300

    js commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
-function selecionar(){
+function select(){
     const element = document.querySelector('li')
+    var list = [document.querySelector('ul')]
 
 element.style.backgroundColor = "hsl(25, 97%, 53%)";
 element.style.opacity = 1;
+
 }
