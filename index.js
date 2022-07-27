8c2fe3f827b9f244760a5fd8f9468ad69b753d62
João Marcelo Dantas <jmgrd98@gmail.com>
2022-07-27 07:55:19 -0300

    commit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+let number = document.getElementsByClassName('number');
 
-const submit = document.getElementById('submit')
-
-submit.addEventListener("click", select)
+number.addEventListener("click", () => {
+    number.setAttribute('value', 'checked');
+    number.style.backgroundColor = 'hsl(25, 97%, 53%)';
+})
 
 function select(){
-    const element = document.getElementsByTagName('label')
+    let number = document.getElementsByClassName('number');
+    let label = document.getElementsByTagName('label');
 
-element.style.backgroundColor = "hsl(25, 97%, 53%)";
-element.style.opacity = 1;
-
-
+    number.setAttribute('value', 'checked');
+    number.style.backgroundColor = 'hsl(25, 97%, 53%)';
 }
 
